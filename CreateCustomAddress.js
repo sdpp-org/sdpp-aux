0d06e40331f02a4b25097b914dc2ba9b6f7d0b70
Curtis Singley <curtissingley@gmail.com>
2019-04-06 22:52:28 -0700

    renamed createCustomAddress to CreateCustomAddress (uppercase)

---
+++ CreateCustomAddress.js
@@ -0,0 +1,34 @@
+///////////////////////////////
+// Create an address from a new seed
+/////
+// First: run this code in a unix based terminal to generate an 81 Tryte seed.
+// 'cat /dev/urandom |LC_ALL=C tr -dc 'A-Z9' | fold -w 81 | head -n 1'
+// Paste the output of the above code into the 'seed' section below.
+///////////////////////////////
+
+function CreateAddress(targetAddress) {
+
+const iotaLibrary = require('@iota/core')
+
+const iota = iotaLibrary.composeAPI({
+  provider: 'https://nodes.devnet.thetangle.org:443'
+})
+
+const seed = targetAddress;
+
+return iota
+  .getNewAddress(seed, { index: 0, total: 1 })
+  .then(address => {
+    console.log('Your address is: ' + address)
+    console.log('Paste this address into https://faucet.devnet.iota.org')
+					return address;
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+let promise = CreateAddress('NEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWWORL9D');
+promise.then((res) =>{
+	console.log(33, 'complete');
+});
