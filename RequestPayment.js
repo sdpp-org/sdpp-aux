325c7b20095449c11374465652d188f65aa55267
Curtis Singley <curtissingley@gmail.com>
2019-04-06 22:25:01 -0700

    adding iota functions

---
+++ RequestPayment.js
@@ -0,0 +1,44 @@
+function RequestPayment(Addresss)
+{
+  const iotaLibrary = require('@iota/core')
+  const Converter = require('@iota/converter')
+
+  const iota = iotaLibrary.composeAPI({
+    provider: 'https://nodes.devnet.thetangle.org:443'
+  })
+
+  // Use a random seed as there is no tokens being sent.
+  const seed =
+    'NEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWWORL9D'
+
+  // Create a variable for the address we will send too
+  const address =Addresss
+   // 'JB9BCZAVZRCHIKUHYERPAW99KFXBSFJXWXPO9PZFIKZBTTCQPQVAKPXUJGNDZHCYYNJBSZWGDTORJUKOB'
+
+  const message = Converter.asciiToTrytes(
+    `This is the addres JB9BCZAVZRCHIKUHYERPAW99KFXBSFJXWXPO9PZFIKZBTTCQPQVAKPXUJGNDZHCYYNJBSZWGDTORJUKOB`
+  )
+
+  const transfers = [
+    {
+      value: 0,
+      address: address, // Where the data is being sent
+      message: message // The message converted into trytes
+    }
+  ]
+
+  return iota
+    .prepareTransfers(seed, transfers)
+    .then(trytes => iota.sendTrytes(trytes, 3, 9))
+    .then(bundle => {
+      console.log('Transfer successfully sent')
+      bundle.map(tx => console.log(tx))
+      return bundle
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+let promise=RequestPayment('JB9BCZAVZRCHIKUHYERPAW99KFXBSFJXWXPO9PZFIKZBTTCQPQVAKPXUJGNDZHCYYNJBSZWGDTORJUKOB');
+promise.then((res)=> {
+console.log(33,'complete');});
---
+++ SendPayment.js
@@ -0,0 +1,47 @@
+// function to send token to specific address
+
+async function SendPayment(targetAddress) {
+    const iotaLibrary = require('@iota/core')
+
+    const iota = iotaLibrary.composeAPI({
+      provider: 'https://nodes.devnet.thetangle.org:443'
+    })
+
+    // Replace this with the seed you sent tokens too!
+    const seed = targetAddress;
+
+    // Generate a different address from your seed
+    const receivingAddress = await iota.getNewAddress(seed, {
+      index: 0,
+      total: 1
+    })
+
+    // Construct a TX to our new address
+    const transfers = [
+      {
+        value: 0,
+        address: receivingAddress[0],
+        tag: 'MYMAGIC'
+      }
+    ]
+    console.log('Sending 0i to ' + receivingAddress)
+
+    try {
+      // Construct bundle and convert to trytes
+      const trytes = await iota.prepareTransfers(seed, transfers)
+      // Send bundle to node.
+      const response = await iota.sendTrytes(trytes, 3, 9)
+
+      console.log('Completed TXs')
+      // response.map(tx => console.log(tx));
+
+      return response;
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const promise = SendPayment('NEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWYASHHNEWEWWORL9D');
+  promise.then((res) => {
+    console.log(33, 'complete');
+  });
